d1ab00e2d1794dc8dc04af238bd0af452878b64e
tkeating <tkeating@click2mail.com>
2019-01-15 17:32:09 -0500

    Exercise 34

---
+++ Section 1/Chapter 3/3.2 Finger Exercises/Finger Exercises.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Finger Exercises|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#| Exercise 34
+String -> Char
+We want the first letter of a given piece of text
+given: "Thomas", expected: "T"
+given: "1@3$!", expected: "1" |#
